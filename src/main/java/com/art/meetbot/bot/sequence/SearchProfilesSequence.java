afe6b5e852baccdd88dfa2fa12613b53fd98c7e3
ifelseelif <ghost.ar@yandex.ru>
2020-11-22 13:33:57 +0300

    added entity MatchedPeople

--- src/main/java/com/art/meetbot/bot/sequence/SearchProfilesSequence.java
+++ src/main/java/com/art/meetbot/bot/sequence/SearchProfilesSequence.java
@@ -6,7 +6,9 @@ import com.art.meetbot.bot.handle.SequenceHandler;
 import com.art.meetbot.bot.util.KeyboardFactory;
 import com.art.meetbot.entity.register.CommandReg;
 import com.art.meetbot.entity.repo.register.CommandRegRepo;
+import com.art.meetbot.entity.repo.user.MatchedPeopleRepo;
 import com.art.meetbot.entity.repo.user.UserRepo;
+import com.art.meetbot.entity.user.MatchedPeople;
 import com.art.meetbot.entity.user.User;
 import com.art.meetbot.entity.user.UserInfo;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +22,6 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Sequence("searching-profiles-seq")
@@ -28,10 +29,12 @@ import java.util.Optional;
 public class SearchProfilesSequence implements SequenceHandler {
     private final CommandRegRepo commandRegRepo;
     private final UserRepo userRepo;
+    private final MatchedPeopleRepo matchedPeopleRepo;
 
-    public SearchProfilesSequence(CommandRegRepo commandRegRepo, UserRepo userRepo) {
+    public SearchProfilesSequence(CommandRegRepo commandRegRepo, UserRepo userRepo, MatchedPeopleRepo matchedPeopleRepo) {
         this.commandRegRepo = commandRegRepo;
         this.userRepo = userRepo;
+        this.matchedPeopleRepo = matchedPeopleRepo;
     }
 
     @Override
@@ -43,20 +46,20 @@ public class SearchProfilesSequence implements SequenceHandler {
                     log.warn("Command reg is not found for chat " + message.getChatId());
                     return new CommandReg();
                 });
+        List<User> users = userRepo.findAll();
+        if (message.getText().equals("accept")) {
+            if (state != 0) {
+                acceptUser(message.getChatId(), users.get(state-1));
+            } else {
+                log.warn("user send accept on 1 question");
+            }
+        }
         if (message.getText().equals("no") || userRepo.count() <= state) {
             commandRegRepo.delete(commandReg);
             return SendMessage.builder()
                     .text("search finished\nFounded:" + (userRepo.count() - 1) + " profile")
                     .chatId(String.valueOf(message.getChatId()))
                     .build();
-        }
-        List<User> users = userRepo.findAll();
-        if (message.getText().equals("accept")) {
-            if (state != 0) {
-                acceptUser(message.getChatId(), users.get(state));
-            } else {
-                log.warn("user send accept on 1 question");
-            }
         }
         if (users.get(state).getTelegramId().equals(message.getChatId().toString())) {
             state += 1;
@@ -90,7 +93,36 @@ public class SearchProfilesSequence implements SequenceHandler {
     }
 
     private void acceptUser(Long chatId, User user) {
+        boolean isMatched = matchedPeopleRepo.findAll().stream()
+                .anyMatch(entity -> entity.getTelegramIdSecond().equals(chatId.toString())
+                        && entity.getTelegramIdFirst().equals(user.getTelegramId()));
 
+        if(isMatched){
+            String reciprocity = "You have reciprocity!\n";
+            String message = makeMessageFromUserInfoWithName(
+                    userRepo.findByTelegramId(chatId.toString()).get().getUserInfo());
+            SendMessage sendMessage = SendMessage.builder()
+                    .text(reciprocity + message)
+                    .chatId(user.getTelegramId())
+                    .build();
+
+            String messageToCurrent = makeMessageFromUserInfoWithName(user.getUserInfo());
+            SendMessage sendMessageToCurrent = SendMessage.builder()
+                    .text(reciprocity + messageToCurrent)
+                    .chatId(chatId.toString())
+                    .build();
+            try {
+                MeetBot.instance.execute(sendMessage);
+                MeetBot.instance.execute(sendMessageToCurrent);
+            } catch (TelegramApiException e) {
+                log.warn("Can't send message to user with chat id" + user.getTelegramId());
+            }
+        }else {
+            MatchedPeople matchedPeople = new MatchedPeople();
+            matchedPeople.setTelegramIdFirst(chatId.toString());
+            matchedPeople.setTelegramIdSecond(user.getTelegramId());
+            matchedPeopleRepo.save(matchedPeople);
+        }
     }
 
     private String makeMessageFromUserInfo(UserInfo userInfo) {
@@ -102,6 +134,17 @@ public class SearchProfilesSequence implements SequenceHandler {
         return "Next user. \nGender:" + userInfo.getSex() +
                 "\nBirth year:" + userInfo.getBirthYear() +
                 "\nDescription:" + description;
+    }
+
+    private String makeMessageFromUserInfoWithName(UserInfo userInfo) {
+        if (userInfo == null) {
+            return "";
+        }
+
+        String message = makeMessageFromUserInfo(userInfo);
+        String description = userInfo.getName() == null ? "" : userInfo.getName();
+
+        return message+  "\nName:" + userInfo.getName();
     }
 
     private void sendPhoto(Long chatId, String photoId) {
@@ -118,7 +161,7 @@ public class SearchProfilesSequence implements SequenceHandler {
         try {
             MeetBot.instance.execute(msg); // Call method to send the photo
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            log.warn("Can't send message to user with chat id" + chatId);
         }
     }
 }
