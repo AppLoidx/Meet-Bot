93b607d9cf5175e822bcb28e902a3ea3997bcd7d
Arthur <apploidyakutsk@gmail.com>
2020-11-22 14:04:32 +0300

    ready to deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker:    java $JAVA_OPTS -jar target/meetbot-0.0.1-SNAPSHOT.jar
--- src/test/java/com/art/meetbot/MeetbotApplicationTests.java
+++ src/test/java/com/art/meetbot/MeetbotApplicationTests.java
@@ -2,7 +2,9 @@ package com.art.meetbot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource(value = "classpath:application.properties")
 @SpringBootTest
 class MeetbotApplicationTests {
 
